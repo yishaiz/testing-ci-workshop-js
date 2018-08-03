3c41c345164c11ee59a7acee229c95dcd6a391a2
yishai <yishai5555@gmail.com>
2018-08-03 17:32:57 +0300

    06-fix-build

--- tests/chai-test-sample.spec.js
+++ tests/chai-test-sample.spec.js
@@ -9,6 +9,6 @@ describe("smoke test - chai", function () {
         expect(true).to.be.true;
 
         //cause test to break
-        expect(true).to.not.be.true;
+        // expect(true).to.not.be.true;
     });
 });
