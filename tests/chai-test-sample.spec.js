f89975717f577463bdbf7b87a2d307dd9a9b2395
yishai <yishai5555@gmail.com>
2018-08-03 17:27:49 +0300

    05-break-build

--- tests/chai-test-sample.spec.js
+++ tests/chai-test-sample.spec.js
@@ -6,7 +6,9 @@ const expect = chai.expect;
 describe("smoke test - chai", function () {
     it("checks equality", function () {
         // assert.equal(true, true);
-        expect (true).to.be.true;
-        // expect (true).to.be.false   ;
+        expect(true).to.be.true;
+
+        //cause test to break
+        expect(true).to.not.be.true;
     });
 });
